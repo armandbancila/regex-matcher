5678253f583748647f31ed631bb4cb2add1a8b9b
Petru-Armand Bancila <petrubancila@gmail.com>
2017-10-15 20:34:32 +0100

    Refactor tabs into spaces

--- re.scala
+++ re.scala
@@ -2,9 +2,9 @@ abstract class Rexp
 case object ZERO extends Rexp
 case object ONE extends Rexp
 case class CHAR(c: Char) extends Rexp
-case class ALT(r1: Rexp, r2: Rexp) extends Rexp   // alternative
-case class SEQ(r1: Rexp, r2: Rexp) extends Rexp   // sequence
-case class STAR(r: Rexp) extends Rexp             // star
+case class ALT(r1: Rexp, r2: Rexp) extends Rexp
+case class SEQ(r1: Rexp, r2: Rexp) extends Rexp
+case class STAR(r: Rexp) extends Rexp
 
 import scala.language.implicitConversions
 import scala.language.reflectiveCalls
@@ -14,6 +14,7 @@ def charlist2rexp(s: List[Char]): Rexp = s match {
   case c::Nil => CHAR(c)
   case c::s => SEQ(CHAR(c), charlist2rexp(s))
 }
+
 implicit def string2rexp(s: String): Rexp = charlist2rexp(s.toList)
 
 implicit def RexpOps (r: Rexp) = new {
@@ -31,122 +32,122 @@ implicit def stringOps (s: String) = new {
 }
 
 def nullable (r: Rexp) : Boolean = r match {
-	case ZERO => {
-		false
-	}
-	case ONE => {
-		true
-	}
-	case CHAR(_) => {
-		false
-	}
-	case ALT(r1, r2) => {
-		nullable(r1) | nullable(r2)
-	}
-	case SEQ(r1, r2) => {
-		nullable(r1) & nullable(r2)
-	}
-	case STAR(_) => {
-		true
-	}
+  case ZERO => {
+    false
+  }
+  case ONE => {
+    true
+  }
+  case CHAR(_) => {
+    false
+  }
+  case ALT(r1, r2) => {
+    nullable(r1) | nullable(r2)
+  }
+  case SEQ(r1, r2) => {
+    nullable(r1) & nullable(r2)
+  }
+  case STAR(_) => {
+    true
+  }
 }
 
 def der (c: Char, r: Rexp) : Rexp = r match {
-	case ZERO => {
-		ZERO
-	}
-	case ONE => {
-		ZERO
-	}
-	case CHAR(d) => {
-		if (c == d) {
-			ONE
-		}
-		else {
-			ZERO
-		}
-	}
-	case ALT(r1, r2) => {
-		ALT(der(c, r1), der(c, r2))
-	}
-	case SEQ(r1, r2) => {
-		if (nullable(r1)) {
-			ALT(SEQ((der(c, r1)), r2), der(c, r2))
-		}
-		else {
-			SEQ(der(c, r1), r2)
-		}
-		
-	}
-	case STAR(r1) => {
-		SEQ(der(c, r1), STAR(r1))
-	}
+  case ZERO => {
+    ZERO
+  }
+  case ONE => {
+    ZERO
+  }
+  case CHAR(d) => {
+    if (c == d) {
+      ONE
+    }
+    else {
+      ZERO
+    }
+  }
+  case ALT(r1, r2) => {
+    ALT(der(c, r1), der(c, r2))
+  }
+  case SEQ(r1, r2) => {
+    if (nullable(r1)) {
+      ALT(SEQ((der(c, r1)), r2), der(c, r2))
+    }
+    else {
+      SEQ(der(c, r1), r2)
+    }
+
+  }
+  case STAR(r1) => {
+    SEQ(der(c, r1), STAR(r1))
+  }
 }
 
 def simp(r: Rexp) : Rexp = r match {
-	case ALT(r1, r2) => (simp(r1), simp(r2)) match {
-		case (x, ZERO) => {
-			x
-		}
-		case (ZERO, x) => {
-			x
-		}
-		case (x, y) if (x == y) => {
-			x
-		}
-		case (x, y) => {
-			ALT(x, y)
-		}
-	}
-	case SEQ(r1, r2) => (simp(r1), simp(r2)) match {
-		case (_, ZERO) => {
-			ZERO
-		}
-		case (ZERO, _) => {
-			ZERO
-		}
-		case (x, ONE) => {
-			x
-		}	
-		case (ONE, x) => {
-			x
-		}
-		case (x, y) => {
-			SEQ(x, y)
-		}
-	}
-	case _ => {
-		r
-	}
+  case ALT(r1, r2) => (simp(r1), simp(r2)) match {
+    case (x, ZERO) => {
+      x
+    }
+    case (ZERO, x) => {
+      x
+    }
+    case (x, y) if (x == y) => {
+      x
+    }
+    case (x, y) => {
+      ALT(x, y)
+    }
+  }
+  case SEQ(r1, r2) => (simp(r1), simp(r2)) match {
+    case (_, ZERO) => {
+      ZERO
+    }
+    case (ZERO, _) => {
+      ZERO
+    }
+    case (x, ONE) => {
+      x
+    }  
+    case (ONE, x) => {
+      x
+    }
+    case (x, y) => {
+      SEQ(x, y)
+    }
+  }
+  case _ => {
+    r
+  }
 }
 
 def ders (s: List[Char], r: Rexp) : Rexp = s match {
-	case Nil => {
-		r
-	}
-	case c::cs => {
-		ders(cs, simp(der(c, r)))
-	}
+  case Nil => {
+    r
+  }
+  case c::cs => {
+    ders(cs, simp(der(c, r)))
+  }
 }
 
 def matcher(r: Rexp, s: String): Boolean = {
-	nullable(ders(s.toList, r))
+  nullable(ders(s.toList, r))
 }
 
 def replace(r: Rexp, s1: String, s2: String): String = {
-	if (!s1.isEmpty){
-		val test = (for (j <- (1 to s1.length).reverse; if (matcher(r, s1.substring(0, j)))) yield s1.substring(0, j))
-		
-		if (!test.isEmpty) {
-			s2 + replace(r, s1.substring(test(0).length, s1.length), s2)
-		}
-		else {
-			s1.substring(0, 1) + replace(r, s1.substring(1, s1.length), s2)
-		}
-	}
-	else {
-		""
-	}
+  if (!s1.isEmpty){
+    val test = (for (j <- (1 to s1.length).reverse; if (matcher(r, s1.substring(0, j)))) yield s1.substring(0, j))
+
+    if (!test.isEmpty) {
+      s2 + replace(r, s1.substring(test(0).length, s1.length), s2)
+    }
+    else {
+      s1.substring(0, 1) + replace(r, s1.substring(1, s1.length), s2)
+    }
+  }
+  else {
+    ""
+  }
 }
 
 val EVIL = SEQ(STAR(STAR(CHAR('a'))), CHAR('b'))
